e92fbb85991e08650d052e2f37cf70b542c0b38c
Aliana Campas <alianacampas@Alianas-MacBook-Pro.local>
2020-12-01 05:36:14 -0700

    .

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3001;
+
+//middlewares
+app.use(cors());
+
+app.get('/', (request,response) => {
+    response.send('Welcome to Image Quiz API');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
