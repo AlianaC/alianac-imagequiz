287f1549546d50b1811f19d96379fb8dbcb8e7c1
Aliana Campas <alianacampas@Alianas-MacBook-Pro.local>
2020-12-01 06:02:39 -0700

    added data

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 var cors = require('cors');
+let data = require('./data');
+
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -9,6 +11,14 @@ app.use(cors());
 app.get('/', (request,response) => {
     response.send('Welcome to Image Quiz API');
 });
+
+app.get('/quizzes', (request,response) => {
+    let metadata = data.quizzes.map(x => {
+        return {id: x.id, name: x.name, picture: x.picture}
+    });
+    response.json(metadata);
+});
+
 
 
 app.listen(port, () => {
